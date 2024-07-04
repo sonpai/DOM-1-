1e9e0534701397e8ce4161802ef62178449312f1
sonpai <115645068+sonpai@users.noreply.github.com>
2024-07-04 20:31:28 +0300

    Ex1.js

---
+++ Ex1.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function addClass(){
+    const text = document.getElementById('text');
+    const change = document.getElementById('classInput').value;
+    text.className=change;
+
+}
